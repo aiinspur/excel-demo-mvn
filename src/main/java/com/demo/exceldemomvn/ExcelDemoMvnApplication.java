c25e5600eeed7613f5acd65bec4879728532ecab
shihujiang <jiangshihu@hotmail.com>
2018-11-28 09:27:49 +0800

    init

---
+++ src/main/java/com/demo/exceldemomvn/ExcelDemoMvnApplication.java
@@ -0,0 +1,12 @@
+package com.demo.exceldemomvn;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ExcelDemoMvnApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ExcelDemoMvnApplication.class, args);
+	}
+}
