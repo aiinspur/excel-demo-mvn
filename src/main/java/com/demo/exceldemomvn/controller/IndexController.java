f8582bf1117f30a23c80d4aff14ad6a5c683a2a2
shihujiang <jiangshihu@hotmail.com>
2018-11-28 19:31:41 +0800

    解析源与模版对应关系

--- src/main/java/com/demo/exceldemomvn/controller/IndexController.java
+++ src/main/java/com/demo/exceldemomvn/controller/IndexController.java
@@ -1,19 +1,33 @@
 package com.demo.exceldemomvn.controller;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.servlet.ModelAndView;
+
+import com.demo.exceldemomvn.service.FileConversion;
 
 @Controller
 @RequestMapping("index")
 public class IndexController {
 
+	@Autowired
+	FileConversion fileConversion;
+
 	@GetMapping
 	public ModelAndView index(Model model) {
 
 		return new ModelAndView("index", "model", model);
+	}
+
+	@ResponseBody
+	@GetMapping("test")
+	public String test(String srcFile, String destinationFile) throws Exception {
+		fileConversion.conversion(srcFile, destinationFile);
+		return "hello test.";
 	}
 
 }
---
+++ src/main/java/com/demo/exceldemomvn/service/ExcelConversion.java
@@ -0,0 +1,97 @@
+package com.demo.exceldemomvn.service;
+
+import static org.springframework.util.Assert.isTrue;
+
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.stereotype.Service;
+import org.springframework.util.ResourceUtils;
+
+import com.demo.exceldemomvn.util.PoiUtil;
+
+@Service
+public class ExcelConversion implements FileConversion {
+
+	private final Logger logger = LoggerFactory.getLogger(ExcelConversion.class);
+
+	@Override
+	public void conversion(String srcFile, String destinationFilePath) throws Exception {
+		File srcFile_ = ResourceUtils.getFile("classpath:" + srcFile);
+		isTrue(srcFile_.exists(), "Src File not exits.");
+
+		if (!new File(destinationFilePath).exists()) {
+			new File(destinationFilePath).mkdirs();
+		}
+
+		Map<String, String> mapping = getMapping(srcFile_,
+				ResourceUtils.getFile("classpath:excel_template/template_1.xls"));
+
+		mapping.forEach((key, val) -> {
+			System.out.println(key + ":" + val);
+		});
+
+	}
+
+	/**
+	 * 获取源文件与模版文件的sheet映射关系. 取交集
+	 * 
+	 * @param srcFile
+	 * @param templateFile
+	 * @return
+	 * @throws IOException
+	 * @throws FileNotFoundException
+	 */
+	private Map<String, String> getMapping(File srcFile, File templateFile) throws Exception {
+		Map<String, String> map = null;
+
+		List<String> templateSheetNameList = PoiUtil.getSheetNameList(templateFile);
+		// 获取转换后的模版sheet页
+		List<String> templateSetList = conversionSheetName(templateSheetNameList);
+
+		List<String> srcSheetNameList = PoiUtil.getSheetNameList(srcFile);
+		// 获取转换后的源文件sheet页
+		List<String> srcSetList = conversionSheetName(srcSheetNameList);
+		// 取交集
+		templateSetList.retainAll(srcSetList);
+
+		map = new HashMap<>(templateSetList.size());
+
+		for (String shortSheetName : templateSetList) {
+			map.put(getSheetNameByShort(shortSheetName, templateSheetNameList),
+					getSheetNameByShort(shortSheetName, srcSheetNameList));
+		}
+
+		return map;
+	}
+
+	private List<String> conversionSheetName(List<String> sheetNameList) {
+		List<String> setList = new ArrayList<>(sheetNameList.size());
+		sheetNameList.forEach(sheetName -> {
+			if (sheetName.length() >= 5) {
+				setList.add(sheetName.substring(0, 5));
+			} else {
+				logger.info("sheetName error." + sheetName);
+			}
+		});
+
+		return setList;
+	}
+
+	private String getSheetNameByShort(String shortName, List<String> sheetNameList) {
+		for (String name : sheetNameList) {
+			if (name.startsWith(shortName)) {
+				return name;
+			}
+		}
+		return "";
+	}
+
+}
